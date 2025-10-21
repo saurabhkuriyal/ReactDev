5a03376d88bb1145b68e4e92b6c9dfefcd6c6c80
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2025-10-21 23:43:31 +0530

    formatting signup page

--- src/app/auth/signup/page.jsx
+++ src/app/auth/signup/page.jsx
@@ -8,20 +8,20 @@ import { useState } from "react";
 
 export default function page() {
 
-  const dispatch=useAppDispatch();
-  const name=useAppSelector((state)=>state.username);
+  const dispatch = useAppDispatch();
+  const name = useAppSelector((state) => state.username);
 
-  const [ formDetails,setFormDetails]=useState({
-    username:"",
-    email:""
+  const [formDetails, setFormDetails] = useState({
+    username: "",
+    email: ""
   })
 
-  
-
-  function handelChange(e){
-    setFormDetails((prevValue)=>{
-      return {...prevValue,
-            [e.target.name]:e.target.value,}
+  function handelChange(e) {
+    setFormDetails((prevValue) => {
+      return {
+        ...prevValue,
+        [e.target.name]: e.target.value,
+      }
     })
 
   }
@@ -30,39 +30,40 @@ export default function page() {
     e.preventDefault();
 
     try {
-      
-      const response=await axios.post("/api/user",formDetails);
 
-      console.log("This is response",response.data.data);
+      const response = await axios.post("/api/user", formDetails);
+
+      console.log("This is response", response.data.data);
 
       dispatch(
         setUser({
-          userId:response.data.data.id,
-          username:response.data.data.username,
-          email:response.data.data.email})
-        )
+          userId: response.data.data.id,
+          username: response.data.data.username,
+          email: response.data.data.email
+        })
+      )
 
-            
-      
+
+
     } catch (error) {
       console.log(error);
-      
+
     }
   }
-  
 
-  console.log("There is the name",name);
 
-  
+  //console.log("There is the name", name);
+
+
   return (
     <div>
-            <h1> Signup form</h1><br />
-            <label htmlFor="username"  >Username</label><br />
-            <input type="text" onChange={handelChange} name="username" placeholder='enter you username' /><br />
-            <label htmlFor="emial">Email</label><br />
-            <input type="text" onChange={handelChange} name="email" placeholder='Enter your email'/><br /><br />c
-            <button type="submit" className="bg-slate-50" onClick={handleSubmit}> Signup</button>
-            <Link href="/auth/login" className="text-white">Login page</Link>
+      <h1> Signup form</h1><br />
+      <label htmlFor="username"  >Username</label><br />
+      <input type="text" onChange={handelChange} name="username" placeholder='enter you username' /><br />
+      <label htmlFor="emial">Email</label><br />
+      <input type="text" onChange={handelChange} name="email" placeholder='Enter your email' /><br /><br />c
+      <button type="submit" className="bg-slate-50" onClick={handleSubmit}> Signup</button>
+      <Link href="/auth/login" className="text-white">Login page</Link>
     </div>
   )
 }
