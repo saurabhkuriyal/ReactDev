6fdecf9d57a688363f9920b2c3edf77b01af1ae0
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2025-10-22 22:30:23 +0530

    commenting consoles in user api

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -4,18 +4,18 @@ import { NextResponse } from "next/server";
 export async function POST(req) {
     try {
 
-        console.log("here is the request",req);
+        //console.log("here is the request",req);
         
         const {username,email}= await req.json();
 
-        console.log("here is the username",username);
-        console.log("here is the email",email);
+        // console.log("here is the username",username);
+        // console.log("here is the email",email);
         
         const user= await prisma.user.create({
             data:{username,email},
         })
 
-        console.log("There is the user",user);
+        //console.log("There is the user",user);
         
         
         return NextResponse.json({status:200 ,success:true,data:user});
