75d7c873400c5f73829c7dd56997187e05ce1e60
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2025-11-13 23:56:41 +0530

    removing gemini and integrating openAI

--- src/configs/AiModel.js
+++ src/configs/AiModel.js
@@ -1,21 +1,61 @@
 
 
-import {
-    GoogleGenAI,
-} from '@google/genai';
+// import {
+//     GoogleGenAI,
+// } from '@google/genai';
 
 
-const ai = new GoogleGenAI({
-    apiKey: process.env.GEMINI_API_KEY,
-});
-const config = {
-    thinkingConfig: {
-        thinkingBudget: -1,
-    },
-    responseMimeType: 'application/json',
-};
-const model = 'gemini-2.5-flash';
+// const ai = new GoogleGenAI({
+//     apiKey: process.env.GEMINI_API_KEY,
+// });
+// const config = {
+//     thinkingConfig: {
+//         thinkingBudget: -1,
+//     },
+//     responseMimeType: 'application/json',
+// };
+// const model = 'gemini-2.5-flash';
 
+
+// //function to generate files
+// export const AiResponse = async (prompt) => {
+
+//     //console.log("My Gemini API Key is:", process.env.GEMINI_API_KEY);
+//     //console.log("Reached here in AI Model page------>");
+
+//     const contents = prompt;
+
+//     //console.log("P--R--O--M--P--T", contents);
+
+
+//     const response = await ai.models.generateContent({
+//         model,
+//         config,
+//         contents,
+
+//     });
+
+//     // console.log("----RESOPONSE----",response);
+//     // console.log("---RESOPONSE----",response.candidates[0].content);
+
+//     // console.log("----RESOPONSE----",response.candidates[0].content.parts[0].text);
+
+
+
+//     return response;
+
+
+// }
+
+import OpenAI from "openai";
+const client = new OpenAI();
+
+// const response = await client.responses.create({
+//     model: "gpt-5-nano",
+//     input: "Write a one-sentence bedtime story about a unicorn."
+// });
+
+// console.log(response.output_text);
 
 //function to generate files
 export const AiResponse = async (prompt) => {
@@ -28,14 +68,14 @@ export const AiResponse = async (prompt) => {
     //console.log("P--R--O--M--P--T", contents);
 
 
-    const response = await ai.models.generateContent({
-        model,
-        config,
-        contents,
+    const response = await client.responses.create({
+    model: "gpt-5-nano",
+    input: contents,
+});
 
-    });
-
-    // console.log("----RESOPONSE----",response);
+    console.log("----RESOPONSE----",response);
+    console.log("here is this ",response.output_text);
+    
     // console.log("---RESOPONSE----",response.candidates[0].content);
 
     // console.log("----RESOPONSE----",response.candidates[0].content.parts[0].text);
@@ -43,6 +83,4 @@ export const AiResponse = async (prompt) => {
 
 
     return response;
-
-
 }
