fd6a81c576375a78ceae650cf59edc6bc2348c0b
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2025-11-15 23:25:30 +0530

    formatting Ai response route

--- src/app/api/AI-response/route.js
+++ src/app/api/AI-response/route.js
@@ -3,25 +3,26 @@ import { AiResponse } from "@/configs/AiModel";
 import { NextResponse } from "next/server";
 export async function POST(req) {
     try {
-        
-        const {Prompt}=await req.json();
+
+        const { Prompt } = await req.json();
 
         const result = await AiResponse(Prompt);
 
         console.log("reached here");
-        
-        // const data=result.candidates[0].content.parts[0].text;
-        const data=result.output_text;
-        console.log("This is response------",data);
 
-        return NextResponse.json({success:true,
-            code:JSON.parse(data)
-            }
+        // const data=result.candidates[0].content.parts[0].text;
+        const data = result.output_text;
+        console.log("This is response------", data);
+
+        return NextResponse.json({
+            success: true,
+            code: JSON.parse(data)
+        }
         );
 
 
     } catch (error) {
         console.log(error);
-        return NextResponse.json({success:false,error})
+        return NextResponse.json({ success: false, error })
     }
 }
