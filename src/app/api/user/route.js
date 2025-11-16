cf89f0a6c35e46b0d7a331ef0439b335e9a83d55
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2025-11-16 23:18:20 +0530

    formatting user route

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -5,24 +5,24 @@ export async function POST(req) {
     try {
 
         //console.log("here is the request",req);
-        
-        const {username,email}= await req.json();
+
+        const { username, email } = await req.json();
 
         // console.log("here is the username",username);
         // console.log("here is the email",email);
-        
-        const user= await prisma.user.create({
-            data:{username,email},
+
+        const user = await prisma.user.create({
+            data: { username, email },
         })
 
         //console.log("There is the user",user);
-        
-        
-        return NextResponse.json({status:200 ,success:true,data:user});
-        
+
+
+        return NextResponse.json({ status: 200, success: true, data: user });
+
     } catch (error) {
-        console.log("There is the error",error);
-        return NextResponse.json({status:500, success:false, error: error.message}, {status: 500});
-        
+        console.log("There is the error", error);
+        return NextResponse.json({ status: 500, success: false, error: error.message }, { status: 500 });
+
     }
 }
